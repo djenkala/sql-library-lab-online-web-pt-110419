37add4c45dc20fbe17ef80e2721729b8cc2ddb46
Daniel Jenkala <djenkala@yahoo.com>
2020-01-26 17:38:30 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,9 +2,9 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
-  subgenre_id INTEGER
-);
-
+  subgenre_id INTEGER,
+  );
+  
 CREATE TABLE subgenres (
   id INTEGER PRIMARY KEY,
   name TEXT
@@ -22,16 +22,16 @@ CREATE TABLE books (
   series_id INTEGER
 );
 
+CREATE TABLE characters (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
+);
+
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
   book_id INTEGER
-);
-
-CREATE TABLE characters (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  species TEXT,
-  motto TEXT,
-  author_id INTEGER
 );
