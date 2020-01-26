eb2ebf99840e03ff8ea8e98fc6470cea336f4b3d
Daniel Jenkala <djenkala@yahoo.com>
2020-01-26 17:56:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,9 +1,16 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "SELECT title, year FROM books WHERE series_id=1 ORDER BY year;"
+  "SELECT title, year 
+    FROM books 
+    WHERE series_id=1 
+    ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
+  "SELECT name, motto
+    FROM characters
+    ORDER BY LENGTH(motto) 
+    DESC 
+    LIMIT 1;"
 end
 
 
@@ -12,7 +19,12 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+    FROM authors 
+    JOIN series 
+    ON series.author_id = authors.id 
+    JOIN subgenres 
+    ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
